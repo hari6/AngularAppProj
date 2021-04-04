bedb9c7071e4eef3a75eb4e3160947cca687abfc
Hari Kishore <vasakishore6@gmail.com>
2021-04-04 09:53:09 +0530

    Go button changes

--- src/app/map-page/map-page.component.ts
+++ src/app/map-page/map-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Location } from "@angular/common"
 
 @Component({
   selector: 'app-map-page',
@@ -8,12 +9,15 @@ import { Router } from '@angular/router';
 })
 export class MapPageComponent implements OnInit {
   public pageTitle = 'Floor Map Page';
-  constructor(private route: Router) {  }
+  constructor(private route: Router,
+    private location: Location) {  }
 
     ngOnInit(){
   
     }
-
+    goBack(){
+      this.location.back();
+  }
   
 
 }
